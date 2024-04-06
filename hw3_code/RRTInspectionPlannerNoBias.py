8d80bec3083ee4cd68c5c483d0c3c9d115bad912
Saleh <salihhassan@campus.technion.ac.il>
2024-04-07 02:32:45 +0300

    base inspection without biasing stats

---
+++ hw3_code/RRTInspectionPlannerNoBias.py
@@ -0,0 +1,128 @@
+import numpy as np
+from RRTTree import RRTTree
+import time
+
+class RRTInspectionPlanner(object):
+
+    def __init__(self, planning_env, ext_mode, goal_prob, coverage):
+
+        # set environment and search tree
+        self.planning_env = planning_env
+        self.tree = RRTTree(self.planning_env, task="ip")
+
+        # set search params
+        self.ext_mode = ext_mode
+        self.goal_prob = goal_prob
+        self.coverage = coverage
+
+    def plan(self):
+        '''
+        Compute and return the plan. The function should return a numpy array containing the states in the configuration space.
+        '''
+        start_time = time.time()
+
+        # initialize an empty plan.
+        plan = []
+        
+        APPEND_INTERMEDIATE_INSPECTIONS = True
+        INTERMEDIATE_MAX_INSPECTIONS = 50
+        INTERMEDIATE_MIN_STEP = np.pi/30
+
+        # TODO: Task 2.4
+        count = 0
+        # Add the start point of the env to the plan 
+        self.tree.add_vertex(self.planning_env.start, self.planning_env.get_inspected_points(self.planning_env.start))
+        counter = 0
+        while self.tree.max_coverage < self.coverage:
+            #goal_bias = np.random.random()
+            #if goal_bias < self.goal_prob: 
+            #    random_config = self.tree.vertices[self.tree.max_coverage_id].config
+            #else:
+            
+            if (counter % 100 == 0):
+                print("iteration, ", counter,end="\t")
+                print("self.tree.max_coverage, ", self.tree.max_coverage)
+            counter += 1
+            
+            # Generate a random config to visit
+            random_config = np.array([np.random.uniform(-np.pi,np.pi) for _ in range(self.planning_env.robot.dim)])
+
+            nearest_state_idx, nearest_config = self.tree.get_nearest_config(random_config)
+            new_config = self.extend(nearest_config, random_config)
+            # Ensure the new config is legal and the edge exists
+            if (self.planning_env.config_validity_checker(new_config) and (self.planning_env.edge_validity_checker(nearest_config, new_config))):
+                
+            
+                # Calculate the new state (combine the inspection points)
+                nearest_inspection_points = self.tree.vertices[nearest_state_idx].inspected_points
+                new_inspection_points = self.planning_env.get_inspected_points(new_config)
+                
+                last_id = nearest_state_idx
+                last_config = nearest_config
+
+                # # This is an improvement for the algorithm that sums up intermediate points for edges
+                # if APPEND_INTERMEDIATE_INSPECTIONS:
+                #     # Get intermediate points
+                #     interpolation_steps = min(int(np.linalg.norm(nearest_config - new_config)//INTERMEDIATE_MIN_STEP), INTERMEDIATE_MAX_INSPECTIONS)   # Hyperparameter 6 for max intermediates
+                #     if interpolation_steps > 0:
+                #         interpolated_configs = np.linspace(start=nearest_config, stop=new_config, num=interpolation_steps)
+                #         for intermediate_config in interpolated_configs:
+                #             new_inspection_points = self.planning_env.compute_union_of_points(new_inspection_points, self.planning_env.get_inspected_points(intermediate_config))
+
+                new_inspection_points = self.planning_env.compute_union_of_points(new_inspection_points, nearest_inspection_points)
+
+                # Add the vertex to the tree
+                new_id = self.tree.add_vertex(config=new_config, inspected_points=new_inspection_points)
+                self.tree.add_edge(last_id, new_id, self.planning_env.robot.compute_distance(last_config, new_config))
+
+        # print total path cost and time
+        curr_idx = self.tree.max_coverage_id
+        start_idx = self.tree.get_root_id()
+
+        while curr_idx != start_idx:
+            nearest_config = self.tree.vertices[self.tree.edges[curr_idx]].config
+            new_config = self.tree.vertices[curr_idx].config
+            print(curr_idx,new_config, self.planning_env.config_validity_checker(new_config), (self.planning_env.edge_validity_checker(nearest_config, new_config)))
+            plan.append(self.tree.vertices[curr_idx].config)
+            curr_idx = self.tree.edges[curr_idx]
+
+        # Add the start state to the plan.
+        plan.append(self.planning_env.start)
+        plan.reverse()
+
+        # print total path cost and time
+        print('Total cost of path: {:.2f}'.format(self.compute_cost(plan)))
+        print('Total time: {:.2f}'.format(time.time()-start_time))
+        print('Inspection coverage', self.tree.max_coverage)
+
+        return np.array(plan)
+
+    def compute_cost(self, plan):
+        '''
+        Compute and return the plan cost, which is the sum of the distances between steps in the configuration space.
+        @param plan A given plan for the robot.
+        '''
+        # TODO: Task 2.4
+
+        cost = 0
+        for i in range(1, len(plan)):
+            cost += self.planning_env.robot.compute_distance(plan[i-1],plan[i])
+        return cost
+
+    def extend(self, near_config, rand_config):
+        '''
+        Compute and return a new configuration for the sampled one.
+        @param near_config The nearest configuration to the sampled configuration.
+        @param rand_config The sampled configuration.
+        '''
+        # TODO: Task 2.4
+
+        n = np.pi/8 # a changeable parameter for step-size
+        if self.ext_mode == "E1" or self.planning_env.robot.compute_distance(near_config, rand_config) < n:
+            return rand_config
+        dist = self.planning_env.robot.compute_distance(near_config, rand_config)
+        normed_direction = (rand_config - near_config) / dist # normed vector
+        new_state = near_config + (n * normed_direction)
+        return new_state
+
+    
--- hw3_code/ip-task-no-bias-plotter.py
+++ hw3_code/ip-task-no-bias-plotter.py
@@ -2,10 +2,11 @@ import numpy as np
 import argparse
 from MapEnvironment import MapEnvironment
 from RRTMotionPlanner import RRTMotionPlanner
-from RRTInspectionPlanner import RRTInspectionPlanner
+from RRTInspectionPlannerNoBias import RRTInspectionPlanner
 import time
 import json
 import matplotlib.pyplot as plt
+import concurrent.futures
 
 
 def plot_performance(goal_bias, avg_costs, avg_times, extension, task, coverage):
@@ -25,15 +26,33 @@ def plot_performance(goal_bias, avg_costs, avg_times, extension, task, coverage)
     else:
         plt.savefig(f'ip-task-no-bias\\{extension}\\coverage={coverage}\\performance_plot_{extension}_coverage={coverage}.png')
 
+def run_with_timeout(planner, timeout):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(planner.plan)
+        try:
+            plan = future.result(timeout=timeout)
+            return plan
+        except concurrent.futures.TimeoutError:
+            print("Planner execution timed out.")
+            return None
+
 def main(args):
     goal_biases = [0.05, 0.2]
     extensions = ['E1', 'E2']
     coverages = [0.5, 0.75]
     all_combinations = [(goal_bias, ext, coverage) for goal_bias in goal_biases for ext in extensions for coverage in coverages]
-    num_executions = 10
+    num_executions, succesful_runs = 10, 0
 
-    for bias, ext, coverage in all_combinations: # for inspection planning
-    # for bias, ext in zip(goal_biases, extensions): # for motion planning
+    for coverage, ext in [(coverage, ext) for coverage in coverages for ext in extensions]: # for inspection planning without bias
+        print(coverage, ext)
+        if coverage == 0.5 and ext == 'E1':
+            continue # already computed
+        if coverage == 0.5 and ext == 'E2':
+            continue # already computed
+        # if coverage == 0.75 and ext == 'E1':
+        #     continue # algorithm doesnt converge!
+        # if coverage == 0.75 and ext == 'E2':
+        #     continue # algorithm doesnt converge!
         costs = []
         times = []
 
@@ -45,32 +64,38 @@ def main(args):
                 planner = RRTMotionPlanner(planning_env=planning_env, ext_mode=ext, goal_prob=bias)
                 save_dir = f'mp-task\\bias={bias}\\{ext}'
             elif args.task == 'ip':
-                planner = RRTInspectionPlanner(planning_env=planning_env, ext_mode=ext, goal_prob=bias, coverage=coverage)
+                planner = RRTInspectionPlanner(planning_env=planning_env, ext_mode=ext, goal_prob=0.05, coverage=coverage)
                 save_dir = f'ip-task-no-bias\\{ext}\\coverage={coverage}'
             else:
                 raise ValueError('Unknown task option: %s' % args.task);
 
             start_time = time.time()
-            plan = planner.plan()
+            
+            plan = run_with_timeout(planner,timeout=1000) # dont allow runs for more than 1000 seconds
             execution_time = time.time() - start_time
-            cost_of_path = planner.compute_cost(plan)
-            planner.planning_env.visualize_plan(plan, save_dir)
+            if plan != None:
+                cost_of_path = planner.compute_cost(plan)
+                planner.planning_env.visualize_plan(plan, save_dir)
 
-            costs.append(cost_of_path)
-            times.append(execution_time)
+                costs.append(cost_of_path)
+                times.append(execution_time)
+                succesful_runs += 1
+            else:
+                costs.append(0)
+                times.append(0)
 
-        avg_cost = sum(costs) / num_executions
-        avg_time = sum(times) / num_executions
+        avg_cost = sum(costs) / succesful_runs
+        avg_time = sum(times) / succesful_runs
         if args.task == 'ip':
-            plot_performance(bias, costs, times, ext, 'ip', coverage)
+            plot_performance(0.05, costs, times, ext, 'ip', coverage)
         else:
             plot_performance(bias, costs, times, ext, 'mp', None)
-        print(f"Goal Bias: {bias * 100}%")
+        print(f"Goal Bias: {0.05 * 100}%")
         print(f"Average Cost of Path: {avg_cost}")
         print(f"Average Execution Time: {avg_time} seconds")
 
         output_data = {
-            "goal_bias": bias,
+            # "goal_bias": bias,
             "average_cost_of_path": avg_cost,
             "average_execution_time": avg_time
         }
